4bb7d9a37ea87440050ab8e3e6a6de36161ab666
Danil2345678 <106225095+Danil2345678@users.noreply.github.com>
2023-07-13 20:04:27 +0300

    Лабораторная работа 1

---
+++ task_2.py
@@ -0,0 +1,9 @@
+disc_size = 1.44 * (1024 ** 2)   # Размер памяти дискеты в байтах
+
+simbol_size = 4
+page_count = 100
+count_string_on_page = 50
+count_simbol_on_page = 25
+
+max_count_book_on_disk = int(disc_size / (page_count * count_string_on_page * count_simbol_on_page * simbol_size))
+print("Количество книг, помещающихся на дискету:", max_count_book_on_disk)
---
+++ task_3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+count_players_on_team = int(len(list_players) / 2)
+team_1 = list_players[:count_players_on_team]
+team_2 = list_players[count_players_on_team:]
+
+print(team_1, team_2, sep="\n")
