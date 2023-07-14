76b5750173618438c866072948e2677b138cd14e
Danil2345678 <106225095+Danil2345678@users.noreply.github.com>
2023-07-14 18:34:33 +0300

    Исправленная лабораторная работа

--- task_1.py
+++ task_1.py
@@ -1,7 +1,7 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-miss_number = (sum(numbers[:4]) + sum(numbers[5:])) / len(numbers)
-numbers[4] = miss_number
+miss_index = 4
+miss_number = (sum(numbers[:miss_index]) + sum(numbers[miss_index + 1:])) / len(numbers)
+numbers[miss_index] = miss_number
 
 print("Измененный список:", numbers)
 
