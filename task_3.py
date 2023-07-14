b32c9c268d5a200722dcfbee6966aa4954b06898
Danil2345678 <106225095+Danil2345678@users.noreply.github.com>
2023-07-14 18:30:22 +0300

    Исправленные задания лабораторной работы

--- task_3.py
+++ task_3.py
@@ -1,5 +1,5 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-count_players_on_team = int(len(list_players) / 2)
+count_players_on_team = len(list_players) // 2
 team_1 = list_players[:count_players_on_team]
 team_2 = list_players[count_players_on_team:]
 
