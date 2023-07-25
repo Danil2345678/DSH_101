3ac35cc121ff280ad922c087fc4d9643c04efec9
Danil2345678 <106225095+Danil2345678@users.noreply.github.com>
2023-07-25 20:51:31 +0300

    Лабораторная работа 2
    Исправления

--- Лабораторная 2/task_1.py
+++ Лабораторная 2/task_1.py
@@ -4,12 +4,16 @@ spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 count_of_month = 0
 
-while money_capital + salary - spend >= 0:
-    money_capital = money_capital + salary - spend
+
+while True:
+    money_capital += salary - spend
     spend = spend + spend * increase
-    count_of_month += 1
-    if money_capital + salary - spend < 0:
-        print("Количество месяцев, которое можно протянуть без долгов:", count_of_month)
+    if money_capital > 0:
+        count_of_month += 1
+    if money_capital < 0:
+        break
+
+print("Количество месяцев, которое можно протянуть без долгов:", count_of_month)
 
 
 
